4d32fdf9d3ca6a45371bc9c39d53ae3f14679982
Nitin Shetye <shetye.nitin13@gmail.com>
2025-03-12 19:45:37 +0530

    changes

--- src/com/ecommerce/Main.java
+++ src/com/ecommerce/Main.java
@@ -2,6 +2,6 @@ package com.ecommerce;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("This is E-commerce Product Management System");
     }
 }
