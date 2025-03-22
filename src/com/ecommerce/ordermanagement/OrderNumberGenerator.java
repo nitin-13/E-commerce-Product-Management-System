1d2667b4e59eebbc10d8473665840160f305fd2f
Nitin Balkrishna Shetye <shetye.nitin13@gmail.com>
2025-03-22 19:50:08 +0530

    changed return statement of order no. generation

--- src/com/ecommerce/ordermanagement/OrderNumberGenerator.java
+++ src/com/ecommerce/ordermanagement/OrderNumberGenerator.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class OrderNumberGenerator {
     public static int generateOrderNumber() {
         Random random = new Random();
-        int orderNumber = 100000 + random.nextInt(900000); // Generates a random 6-digit number
-        return orderNumber;
+        return  100000 + random.nextInt(900000); // Generates a random 6-digit number
     }
 }
